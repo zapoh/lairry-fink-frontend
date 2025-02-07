a9ec6548c3e97ca42cca85f5abd81164d483b071
Fahim <fahimzapoh@gmail.com>
2025-02-07 07:16:49 +0500

    favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,9 @@ const Web3ProviderNoSSR = dynamic(
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Web3ProviderNoSSR>
+      <Head>
+        <title>L(ai)rry Fink: AI-controlled ETF</title>
+      </Head>
       <main className={inter.className}>
         <Component {...pageProps} />
       </main>
